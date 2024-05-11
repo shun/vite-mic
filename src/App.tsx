0c5fe6852e3d93bdc8dfb533f0deb2d0e799e14a
KUDO Shunsuke <skudo@skudo-mbp.local>
2024-05-12 02:01:18 +0900

    UIを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Button, Flex, Textarea, Heading } from "@chakra-ui/react";
+import { useState } from "react";
+import { useAudioRecord, useWhisper, useChatGPT } from "./hooks";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { recording, source, startRecording, stopRecording } = useAudioRecord();
+  const { transcribeText, transcribe } = useWhisper();
+  const [llmtext, setLlmtext] = useState<string>("");
+  const { start } = useChatGPT(setLlmtext);
+  const handleOnClickGPT = () => {
+    setLlmtext("");
+    const inputtext = `
+### 指示
+入力されたテキストをGitHub フレーバのマークダウン形式に整形してください。
+見出しを使用して主要なセクションを区切り、重要なポイントは太字で強調してください。
+情報がリスト形式で表現できる部分は箇条書きを用いてください。
+### 入力
+${transcribeText}`;
+    start(inputtext);
+  };
+
+  const handleOnClickStop = () => {
+    stopRecording();
+    transcribe(source);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box alignItems="center">
+      <Flex m={8} alignItems="center" justifyContent="center">
+        {!recording && !source && (
+          <Button m={2} colorScheme="blue" onClick={startRecording}>
+            Record
+          </Button>
+        )}
+        {recording && (
+          <Button m={2} colorScheme="red" onClick={handleOnClickStop}>
+            Stop
+          </Button>
+        )}
+        {source && (
+          <Button m={2} colorScheme="blue" onClick={() => transcribe(source)}>
+            Transcribe
+          </Button>
+        )}
+        {transcribeText && (
+          <Button m={2} colorScheme="blue" onClick={handleOnClickGPT}>
+            GPT
+          </Button>
+        )}
+      </Flex>
+      <Flex m={8} alignItems="center" justifyContent="center">
+        {typeof source === "string" && <audio src={source} controls />}
+      </Flex>
+      <Flex m={8} alignItems="center" justifyContent="center">
+        <Textarea
+          m={4}
+          rows={6}
+          placeholder="Whisper Response"
+          value={transcribeText}
+        />
+      </Flex>
+      {llmtext && (
+        <Box mx={16}>
+          <Heading as="h2" my={2}>
+            要約
+          </Heading>
+          <Box>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{llmtext}</ReactMarkdown>
+          </Box>
+        </Box>
+      )}
+    </Box>
+  );
 }
 
-export default App
+export default App;
